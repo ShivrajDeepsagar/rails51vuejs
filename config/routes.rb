80b668ada8ed36ef1874f84abbbd66bb6999fb25
Shivraj Deepsagar <shivaraj.deepasagar@gmail.com>
2018-04-17 14:51:26 +0530

    api namespace versioning

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
-  resources :authors do
-    resources :books
+  namespace :api do
+    namespace :v1 do
+      resources :authors do
+        resources :books
+      end
+    end
   end
+  
   root 'authors#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
