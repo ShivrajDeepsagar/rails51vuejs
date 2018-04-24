689c862213d3c87cacb28afb15e674a62473108b
Shivraj Deepsagar <shivaraj.deepasagar@gmail.com>
2018-04-24 17:17:53 +0530

    fixed user creation route error

--- app/controllers/api/v1/auths_controller.rb
+++ app/controllers/api/v1/auths_controller.rb
@@ -1,12 +1,13 @@
 module Api
   module V1
     class AuthsController < ApplicationController
+      skip_before_action :authenticate_user, raise: false
       def create
         token_command = AuthenticateUserCommand.call(*params.slice(:user, :password).values)
         if token_command.success?
           render json: {token: token_command.result }
         else
-          render json: {error: token_command.errors }, status: :unauthorized 
+          render json: {error: token_command.errors }, status: :unauthorized
         end
       end
     end
