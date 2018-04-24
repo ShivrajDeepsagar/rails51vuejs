f65cc17181fd885ddeaef47db779d6ab1acd7c57
Shivraj Deepsagar <shivaraj.deepasagar@gmail.com>
2018-04-24 17:14:07 +0530

    authenticating tokens

---
+++ app/controllers/concerns/token_authenticable.rb
@@ -0,0 +1,15 @@
+class NotAuthorizedException < StandardError; end
+module TokenAuthenticable
+  extend ActiveSupport::Concern
+  include do
+    attr_reader :current_user
+    before_action :authenticate_user
+    rescue_from NotAuthorizedException, with -> {render json: {error: 'Not authorized'}, status: 401}
+  end
+
+  private
+  def authenticate_user
+    @current_user = DecodeAuthenticationCommand.call(request.headers).result
+    raise NotAuthorizedException unless @current_user
+  end
+end
